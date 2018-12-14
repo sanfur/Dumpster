dfd65478f6d434d2d71934e4f3d7375ad22dea91
Sandro <sandro.fuerer@gmail.com>
2018-12-14 11:35:57 +0100

    Added toilets to map

--- map.js
+++ map.js
@@ -8,6 +8,7 @@
     	        navigator.geolocation.getCurrentPosition(
     	            function(position){
     	            	generateMap(position.coords)
+    	            	findToilets(position.coords)
     	            }
     	        );
     	    } else {
@@ -17,8 +18,12 @@
       
       function generateMap(coords){
 
-    	  	var latitude = coords.latitude;
-    	  	var longitude = coords.longitude;
+    	  // Temporary lat long 9.53287, 46.84986
+//    	  var latitude = coords.latitude;
+    	  var latitude = 46.84986;
+//    	  var longitude = coords.longitude;
+    	  var longitude = 9.53287;
+    	  	
     	  	map = new OpenLayers.Map("basicMap");
 			var mapnik        	= new OpenLayers.Layer.OSM();
 			var fromProjection 	= new OpenLayers.Projection("EPSG:4326"); 
@@ -36,25 +41,51 @@
 			map.setCenter(position, zoom );
       }
 	  
-	  $.ajax({
-			url:
-				'https://www.overpass-api.de/api/interpreter?data=' + 
-				'[out:json][timeout:60];' + 
-				'area["boundary"~"administrative"]["name"~"Berlin"];' + 
-				'node(area)["amenity"~"school"];' + 
-				'out;',
-			dataType: 'json',
-			type: 'GET',
-			async: true,
-			crossDomain: true
-		}).done(function() {
-			console.log( "second success" );
-		}).fail(function(error) {
-			console.log(error);
-			console.log( "error" );
-		}).always(function() {
-			console.log( "complete" );
-		});
+      function findToilets(coords){
+    	  
+    	  var dif = 0.1;
+//    	  var minLat = coords.latitude - dif;
+//    	  var minLon = coords.longitude - dif;
+//    	  var maxLat = coords.latitude + dif;
+//    	  var maxLon = coords.longitude + dif;
+    	  
+    	  var minLat = 46.84986 - dif;
+    	  var minLon = 9.53287 - dif;
+    	  var maxLat = 46.84986 + dif;
+    	  var maxLon = 9.53287 + dif;
+    	  
+    	  
+    	  $.ajax({
+	  			url:
+	  				'https://www.overpass-api.de/api/interpreter?data=' + 
+	  				'[out:json][timeout:60];' + 
+	  				'node [amenity=toilets]' + 
+	  				'(' + minLat + ',' + minLon +',' + maxLat + ',' + maxLon + ');' + 
+	  				'out;',
+	  			dataType: 'json',
+	  			type: 'GET',
+	  			async: true,
+	  			crossDomain: true,
+	  			success: function(data) {
+					$(data.elements).each(function(index, value){											
+						var fromProjection 	= new OpenLayers.Projection("EPSG:4326"); 
+						var toProjection   	= new OpenLayers.Projection("EPSG:900913");
+						var size 			= new OpenLayers.Size(30,30);
+						var icon 			= new OpenLayers.Icon('toilet.png', size);   
+						var position       	= new OpenLayers.LonLat(value.lon, value.lat).transform( fromProjection, toProjection);       
+						var markers = new OpenLayers.Layer.Markers( "Markers" );
+						map.addLayer(markers);
+						markers.addMarker(new OpenLayers.Marker(position, icon));
+					});	
+				},	  				
+	  			error: function(error) {
+	  			console.log(error);
+	  			console.log( "error" );
+	  			},
+	  		});
+      }
+      
+	 
 
       
 
