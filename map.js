70701b88fed16f45abe5afdf5b607f469907af4f
Sandro Fürer <sandro.fuerer@gmail.com>
2018-12-15 00:26:08 +0100

    Finished Dumpster
    - handle hotspot and toilet coordinates
    - improve icons
    - manage google maps navigation

--- map.js
+++ map.js
@@ -1,28 +1,36 @@
 
-      function clickBack() {
-          location.href = 'startPoint.html';
-      };    
-      
-      function waitForInput(callback) {
+	  // Global variables
+	  var latitudeSimulation = 51.508530;
+	  var longitudeSimulation = -0.076132;
+      var wifis = [];
+      var map;
+      var currentPosition;
+
+      // Initialize
+      window.onload = function (callback) {
     	    if (navigator.geolocation) {
     	        navigator.geolocation.getCurrentPosition(
     	            function(position){
-    	            	generateMap(position.coords)
-    	            	findToilets(position.coords)
+    	            	generateMap(position.coords);
+    	            	findHotspots(position.coords);
+    	            	findToilets(position.coords);
     	            }
     	        );
     	    } else {
     	      return"Unknown";
     	    }
       }
+
+      // Functions
+      function clickBack() {
+    	  location.href = 'startPoint.html';
+      };  
       
       function generateMap(coords){
 
-    	  // Temporary lat long 9.53287, 46.84986
-//    	  var latitude = coords.latitude;
-    	  var latitude = 46.84986;
-//    	  var longitude = coords.longitude;
-    	  var longitude = 9.53287;
+    	  currentPosition = coords;
+    	  var latitude = coords.latitude;
+    	  var longitude = coords.longitude;
     	  	
     	  	map = new OpenLayers.Map("basicMap");
 			var mapnik        	= new OpenLayers.Layer.OSM();
@@ -31,36 +39,60 @@
 			var position       	= new OpenLayers.LonLat(longitude, latitude).transform( fromProjection, toProjection);       
 			var size 			= new OpenLayers.Size(30,30);
 			var icon 			= new OpenLayers.Icon('marker.png', size);   
-			var zoom           	= 15;         
+			var zoom           	= 15;   
 			map.addLayer(mapnik);
 			
 			var markers = new OpenLayers.Layer.Markers( "Markers" );
 			map.addLayer(markers);
-			markers.addMarker(new OpenLayers.Marker(position, icon));
+			var currentMarker = new OpenLayers.Marker(position, icon); 
+			markers.addMarker(currentMarker);
 			
 			map.setCenter(position, zoom );
       }
 	  
-      function findToilets(coords){
-    	  
+      function findHotspots(coords){
     	  var dif = 0.1;
-//    	  var minLat = coords.latitude - dif;
-//    	  var minLon = coords.longitude - dif;
-//    	  var maxLat = coords.latitude + dif;
-//    	  var maxLon = coords.longitude + dif;
     	  
-    	  var minLat = 46.84986 - dif;
-    	  var minLon = 9.53287 - dif;
-    	  var maxLat = 46.84986 + dif;
-    	  var maxLon = 9.53287 + dif;
-    	  
+    	  $.ajax({
+	  			url:
+	  				'https://www.overpass-api.de/api/interpreter?data=' + 
+	  				'[out:json][timeout:60];' + 
+	  				'node [internet_access=wlan]' + 
+	  				'(' + 
+	  				(coords.latitude - dif) + ',' + 
+	  				(coords.longitude - dif) +',' + 
+	  				(coords.latitude + dif) + ',' + 
+	  				(coords.longitude + dif) + ');' + 
+	  				'out;',
+	  			dataType: 'json',
+	  			type: 'GET',
+	  			async: true,
+	  			crossDomain: true,
+	  			success: function(data) {
+	  				 $(data.elements).each(function(index, value){
+	  					 wifis[index] = value;
+	  				 });	
+				},	  				
+	  			error: function(error) {
+	  			console.log(error);
+	  			console.log( "error" );
+	  			}
+	  		});
+      }
+      
+      function findToilets(coords){
+    	  var dif = 0.1;
     	  
     	  $.ajax({
 	  			url:
 	  				'https://www.overpass-api.de/api/interpreter?data=' + 
 	  				'[out:json][timeout:60];' + 
 	  				'node [amenity=toilets]' + 
-	  				'(' + minLat + ',' + minLon +',' + maxLat + ',' + maxLon + ');' + 
+	  				'(' + 
+	  				(coords.latitude - dif) + ',' + 
+	  				(coords.longitude - dif) +',' + 
+	  				(coords.latitude + dif) + ',' + 
+	  				(coords.longitude + dif) + ');' + 
 	  				'out;',
 	  			dataType: 'json',
 	  			type: 'GET',
@@ -68,14 +100,8 @@
 	  			crossDomain: true,
 	  			success: function(data) {
 					$(data.elements).each(function(index, value){											
-						var fromProjection 	= new OpenLayers.Projection("EPSG:4326"); 
-						var toProjection   	= new OpenLayers.Projection("EPSG:900913");
-						var size 			= new OpenLayers.Size(30,30);
-						var icon 			= new OpenLayers.Icon('toilet.png', size);   
-						var position       	= new OpenLayers.LonLat(value.lon, value.lat).transform( fromProjection, toProjection);       
-						var markers = new OpenLayers.Layer.Markers( "Markers" );
-						map.addLayer(markers);
-						markers.addMarker(new OpenLayers.Marker(position, icon));
+						
+						hotspotAndToiletMatches(value)
 					});	
 				},	  				
 	  			error: function(error) {
@@ -85,19 +111,54 @@
 	  		});
       }
       
-	 
+	  function hotspotAndToiletMatches(coords){
+		  
+		  var fromProjection = new OpenLayers.Projection("EPSG:4326"); 
+		  var toProjection = new OpenLayers.Projection("EPSG:900913");
+		  var markers = new OpenLayers.Layer.Markers( "Markers" );
+		  map.addLayer(markers);
+		  var position = new OpenLayers.LonLat(coords.lon, coords.lat).transform( fromProjection, toProjection);       
+          var size = new OpenLayers.Size(45,55);
 
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
+		  var i;
+		  for(i = 0; i < wifis.length; i++){
+				var pointToilet = new OpenLayers.Geometry.Point(coords.lon, coords.lat);
+				var pointWifi = new OpenLayers.Geometry.Point(wifis[i].lon, wifis[i].lat)
+				var distance = pointToilet.distanceTo(pointWifi);
+				
+				if(distance < 0.001){
+					  var icon = new OpenLayers.Icon('hotspotToiletMarker.png', size); 
+					  var marker = new OpenLayers.Marker(position, icon);
+					  markers.addMarker(marker);            
+					  markers.events.register( 'click', marker, function(marker){ 
+						  	prepareNavigationButton(marker, fromProjection, toProjection);
+					  });
+					  return;
+				}	 
+		  }
+		  
+		  var icon = new OpenLayers.Icon('toiletMarker.png', size);  
+		  var marker = new OpenLayers.Marker(position, icon);		  
+		  markers.addMarker(marker);		  
+		  markers.events.register( 'click', marker, function(marker){ 
+			  	prepareNavigationButton(marker, fromProjection, toProjection);
+		  });
+	  }
+	  
+	  function prepareNavigationButton(marker, fromProjection, toProjection){
+		  var position = new OpenLayers.LonLat(
+				  marker.object.markers[0].lonlat.lon, 
+				  marker.object.markers[0].lonlat.lat).transform( toProjection, fromProjection
+		  );       
+		  var button = document.getElementById("findRoute");
+		  button.href = "http://maps.google.com/maps?saddr=" +
+		  				currentPosition.latitude + 
+		  				"," + 
+		  				currentPosition.longitude + 
+		  				"&daddr=" + 
+		  				position.lat + 
+		  				"," + 
+		  				position.lon;
+		  button.style.display = "block";
+	  }
+	  
